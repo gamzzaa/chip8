90dd198fa9e2650fc6ea769354f3a6c5280d611f
Marcin Borowiec <gamzzaa@gmail.com>
2017-04-08 00:26:56 +0200

    Add registers

--- cpu.h
+++ cpu.h
@@ -3,7 +3,29 @@
 
 namespace cpu
 {
-
+  typedef char u8;
+  typedef short int u16;
+  
+  struct registers
+  {
+    u8 V0;
+    u8 V1;
+    u8 V2;
+    u8 V3;
+    u8 V4;
+    u8 V5;
+    u8 V6;
+    u8 V7;
+    u8 V8;
+    u8 V9;
+    u8 VA;
+    u8 VB;
+    u8 VC;
+    u8 VD;
+    u8 VE;
+    u8 VF;
+    u16 I;
+  };
 }
 
 #endif //> CPU_H_
