086707606ace3aadd72b46467fcdf402f8d7cb7e
Marcin Borowiec <gamzzaa@gmail.com>
2017-04-08 23:20:32 +0200

    Add memory map in comment section

--- memory.h
+++ memory.h
@@ -1,6 +1,32 @@
 #ifndef MEMORY_H_
 #define MEMORY_H_
 
+/**
+Memory Map:
++---------------+= 0xFFF (4095) End of Chip-8 RAM
+|               |
+|               |
+|               |
+|               |
+|               |
+| 0x200 to 0xFFF|
+|     Chip-8    |
+| Program / Data|
+|     Space     |
+|               |
+|               |
+|               |
++- - - - - - - -+= 0x600 (1536) Start of ETI 660 Chip-8 programs
+|               |
+|               |
+|               |
++---------------+= 0x200 (512) Start of most Chip-8 programs
+| 0x000 to 0x1FF|
+| Reserved for  |
+|  interpreter  |
++---------------+= 0x000 (0) Start of Chip-8 RAM
+ */
+
 namespace memory
 {
 
