00d607eb518dac8be885e4df951ff3dae89e50b0
Marcin Borowiec <gamzzaa@gmail.com>
2017-04-08 23:12:50 +0200

    Add memory header

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "cpu.h"
+#include "memory.h"
 
 int main()
 {
   short int i = 0;
   std::cout << "chip8 start" << std::endl;
+
+  memory::ram[0] = 0;
+  
   std::cout << sizeof(i) << std::endl;
   return 0;
 }
---
+++ memory.h
@@ -0,0 +1,14 @@
+#ifndef MEMORY_H_
+#define MEMORY_H_
+
+namespace memory
+{
+
+  typedef char u8;
+  static const unsigned int MEM_SIZE = 4096;
+
+  u8 ram[MEM_SIZE];
+
+}
+
+#endif
